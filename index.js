fee4187db397d5a6eeb8e0a6a7648ae5e242921b
Manish Bourai <manish.bourai@completrix.com>
2026-01-02 19:04:55 +0530

    add frontend api

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ connectDB();
 
 const app = express();
 app.use(cors({
-    origin: "http://localhost:5173"|| "*",
+    origin: "https://contact-frontend-gules.vercel.app/"|| "*",
     credentials: true  
 }));
 app.use(express.json());

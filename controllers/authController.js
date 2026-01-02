af5543e5a03c06bfb186f42a32e455bf46d83fdd
manishBourai <zorogodno5@gmail.com>
2026-01-02 19:32:47 +0530

    fix

--- controllers/authController.js
+++ controllers/authController.js
@@ -28,7 +28,7 @@ export const login = async (req, res) => {
   if (!user)
     return res.status(400).json({ message: "User not found" });
 
-  const isMatch =  bcrypt.compare(password, user.password);
+  const isMatch = await bcrypt.compare(password, user.password);
   if (!isMatch)
     return res.status(400).json({ message: "Invalid password" });
 
